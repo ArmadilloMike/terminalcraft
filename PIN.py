06fca4ffe9e8534545d7066522f24f5b2fd94757
JacksonWengler <jacksonwengler@gmail.com>
2025-03-19 13:09:49 -0500

    Update PIN.py
    Added ability to hash the pin for safer storage

--- PIN.py
+++ PIN.py
@@ -12,8 +12,11 @@ def request_pin():
         try:
             pin = int(input("Please enter your 4-digit PIN: "))
             if 0000 <= pin <= 9999:
+                hashed_pin = hash_pin(pin)
+                print(hashed_pin)
                 return pin
             else:
                 print("Invalid PIN. Please enter a 4-digit number.")
         except ValueError:
             print("Invalid input. Please enter a number.")
+request_pin()
